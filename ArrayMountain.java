56c9b292f9b10ded260e3532dea066ecda7c540b
Nishi Agrawal <Nishi@macs-MacBook-Air.local>
2021-07-22 15:57:37 +0530

    added ArrayMountain solution

---
+++ ArrayMountain.java
@@ -0,0 +1,42 @@
+
+public class ArrayMountain {
+	
+	public static boolean validMountainArray(int[] arr) {
+        if(arr.length < 3){
+            return false;
+        }
+        int previous = arr[0];
+        int i = 1;
+        boolean check = false;
+        while(previous < arr[i]){
+            if(i+1 > arr.length-1){
+                return false;
+            }
+            check = true;
+            previous = arr[i];
+            i++;
+        }
+        boolean flag = false;
+        while(previous > arr[i]){
+        	flag = true;
+            if((i+1) <= (arr.length-1) ){
+                previous = arr[i];
+                i++;
+                if(previous < arr[i]) {
+                	return false;
+                }
+            }
+            else break;
+            
+        }
+        
+        return (check == true && flag == true)? true : false;
+    }
+
+	public static void main(String[] args) {
+		int [] arr = {0,3,2,1};
+		System.out.println(validMountainArray(arr));
+
+	}
+
+}
